f7a8b30af1b0a6ee1f34db0230871514b01814ba
Joachim <joachim.happel@gmail.com>
2025-09-02 21:30:20 +0200

    fixe tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/systhemische-beratung/',
+      base: command === 'build' ? '/systhemische-beratung/' : '/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
